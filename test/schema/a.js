7e09ace269045c9bdb4a7b60fa6f01df7dd7c08d
蘇銘峰 <kudokawai@gmail.com>
2017-07-26 11:18:05 +0800

    change: update docs

--- test/schema/a.js
+++ test/schema/a.js
@@ -3,12 +3,19 @@
  * 並且將B引用進來
  */
 import mongoose from "mongoose"
+import {
+  GraphQLFloat
+} from "graphql"
 import b from "./b";
 
 let Schema = mongoose.Schema;
 export default Schema({
   stringType: String,
   numberType: Number,
+  floatType: {
+    type: Number,
+    graphql: GraphQLFloat
+  },
   dateType: Date,
   requiredType: {
     required: true,
